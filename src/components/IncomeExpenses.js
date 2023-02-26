40613b36d4c651fa74a6973b3694200a13a7a2ee
wsarsou <walaahsarsour@gmail.com>
2023-02-26 10:13:03 -0500

    added use&global state management+added dummy income

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const IncomeExpenses = () => {
   return (
     <div className="inc-exp-container">
-    <div>
-      <h4>Income</h4>
-<p id="money plus" class="money plus">$0.00</p>
+      <div>
+        <h4>Income</h4>
+        <p id="money plus" class="money plus">
+          $0.00
+        </p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money minus" class="money minus">
+          -$0.00
+        </p>
+      </div>
     </div>
-    <div>
-      <h4>Expense</h4>
-<p id="money minus" class="money minus">-$0.00</p>
-    </div>
-  </div>
-  )
-}
+  );
+};
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-    return (
-        <>
-        <h3>History</h3>
-        <ul id="list" class="list">
-            <li class="minus">
-                Cash <span>-$400</span><button class="delete-btn">x</button>
-            </li>
-        </ul>
-        </>
-    )
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
+  )
 }
